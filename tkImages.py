b7f2f98afa1213f0debec7701b8c48edb0b8e24d
Dnislam Urazayev <52580585+prinnevald@users.noreply.github.com>
2020-06-21 23:23:43 +0600

    Add files via upload

---
+++ tkImages.py
@@ -0,0 +1,15 @@
+from tkinter import *
+
+# main function
+
+def main():
+        
+    # creating window
+    
+    root = Tk()
+    root.title("tkImages")
+        
+    root.mainloop()
+    
+if __name__ == "__main__":
+    main()
