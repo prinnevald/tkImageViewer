95c1c052b3eb1fe8d34d01bc7b6ee35eab570cb2
Dnislam Urazayev <52580585+prinnevald@users.noreply.github.com>
2020-06-22 17:55:19 +0600

    cosmetic changes and status bar

--- tkImageViewer.py
+++ tkImageViewer.py
@@ -13,17 +13,23 @@ class Memory():
 
 # change the image functions
 
-def prevImg(label, mem):
+def prevImg(status, label, mem):
     mem.cursor = mem.cursor - 1
     if mem.cursor == -1:
         mem.cursor = mem.cursor + 1
-    label.configure(image=mem.images[mem.cursor])
 
-def nextImg(label, mem):
-    mem.cursor = mem.cursor + 1
-    if mem.cursor == len(os.listdir("images")):
-        mem.cursor = mem.cursor - 1
     label.configure(image=mem.images[mem.cursor])
+    stat = mem.cursor + 1
+    status.configure(text="Image "+str(stat)+" of "+str(len(mem.images)))
+
+def nextImg(status, label, mem):
+    mem.cursor = mem.cursor + 1
+    if mem.cursor == len(mem.images):
+        mem.cursor = mem.cursor - 1
+
+    label.configure(image=mem.images[mem.cursor])
+    stat = mem.cursor + 1
+    status.configure(text="Image "+str(stat)+" of "+str(len(mem.images)))
 
 ### MAIN ###
 
@@ -47,15 +53,25 @@ def main():
     label = Label(image=mem.images[mem.cursor])
     label.grid(column=0, row=0, columnspan=3)
 
+    # creating status bar
+
+    status = Label(root, text="Image 1 of "+str(len(mem.images)),
+                    bd=1, relief=SUNKEN, anchor=E)
+
     # buttons for changing images and quitting
 
-    b_prev = Button(root, text="Previous", command=lambda:prevImg(label, mem))
-    b_next = Button(root, text="Next", command=lambda:nextImg(label, mem))
-    b_quit = Button(root, text="Exit Program", command=root.quit)
+    p = "<< Previous Image"
+    n = "Next Image >>"
+    e = "Exit Viewer"
+
+    b_prev = Button(root, text=p, command=lambda:prevImg(status, label, mem))
+    b_next = Button(root, text=n, command=lambda:nextImg(status, label, mem))
+    b_quit = Button(root, text=e, command=root.quit)
     
     b_prev.grid(column=0, row=1)
     b_next.grid(column=2, row=1)
-    b_quit.grid(column=1, row=1)
+    b_quit.grid(column=1, row=1, pady=10)
+    status.grid(column=0, row=2, columnspan=3, sticky=W+E)
 
     root.mainloop()
     
